c1d8124731d6472520dcc5496a85cc1cb29802e6
Michael Trichilo <michaeltrichilo3@gmail.com>
2018-02-13 23:20:35 -0500

    Only two cards can be selected at a time

--- lib/memory/memory_game.ex
+++ lib/memory/memory_game.ex
@@ -11,7 +11,7 @@ defmodule Memory.Game do
     letters = Enum.shuffle(letters ++ letters)
 
     board = Enum.map(letters, &(%{letter: &1, matched: false, selected: false}))
-    %{board: board, clicks: 0}
+    %{board: board, clicks: 0, wait: false}
   end
 
   def xnor(first, second) do
@@ -22,18 +22,20 @@ defmodule Memory.Game do
   State logic when a player selects a card.
   """
   def select(state, index) do
-    if Enum.at(state.board, index).selected do
+    if Enum.at(state.board, index).selected || state.wait do
       state
     else
       board = List.replace_at(state.board, index, %{Enum.at(state.board, index) | selected: true})
-      update = %{board: board, clicks: state.clicks + 1}
+      update = %{board: board, clicks: state.clicks + 1, wait: state.wait}
       letter = Enum.at(update.board, index).letter
       if Enum.count(update.board, &(&1.selected)) == 2 do
-        if Enum.all?(update.board, &(xnor(&1.selected, &1.letter == letter))) do
-          %{update | board: Enum.map(update.board, &(%{&1 | matched: &1.matched || &1.letter == letter}))}
-        else
-          update
-        end
+        temp =
+          if Enum.all?(update.board, &(xnor(&1.selected, &1.letter == letter))) do
+            temp = %{update | board: Enum.map(update.board, &(%{&1 | matched: &1.matched || &1.letter == letter}))}
+          else
+            update
+          end
+        %{temp | wait: true}
       else
         update
       end
@@ -44,6 +46,6 @@ defmodule Memory.Game do
   Flips any selected cards back over.
   """
   def flip(state) do
-     %{board: Enum.map(state.board, &(%{&1 | selected: false})), clicks: state.clicks}
+     %{board: Enum.map(state.board, &(%{&1 | selected: false})), clicks: state.clicks, wait: false}
   end
 end
